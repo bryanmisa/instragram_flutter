6d0daca7b7a042208ee0c2c418d94f9a840bd7a2
Bryan Misa <bryan.misa@gmail.com>
2023-11-18 20:05:28 +0300

    update 8 files

--- README.md
+++ README.md
@@ -6,4 +6,18 @@
     - cloud_firestore
     - firebase_auth 
     - firebase_storage
-    
+ 2. Save the google-services.json from firebase storage
+ 3. Add add the code below on the project/build.gradle on the dependencies
+    dependencies {
+        classpath "com.google.gms:google-services:4.4.0"
+
+    }
+ 4. Add the following code on bundle.gradle project/app
+dependencies {
+    implementation platform('com.google.firebase:firebase-bom:32.6.0')
+}
+
+5. Install nodejs
+6. run firebase login
+7. dart pub global activate flutterfire_cli
+8. 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instragram_flutter/firebase_options.dart';
 import 'package:instragram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instragram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instragram_flutter/responsive/web_screen_layout.dart';
 import 'package:instragram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //ensure that the flutter widget has been initialized
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyA4GMlVpK3SWmiRKzVVDvaVmPG571O_RKU",
+        appId: "1:43655460223:web:6bd5d619c1945c11c4964a",
+        messagingSenderId: "43655460223",
+        projectId: "instagram-tutorial-95668",
+        storageBucket: "instagram-tutorial-95668.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
   runApp(const MyApp());
 }
 
